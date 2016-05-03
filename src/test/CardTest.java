f95cdb7aa6b65b1698a5130a2b9f24b80f402527
cruiter1 <cruiter1@asu.edu>
2016-05-03 16:44:54 -0600

    Test Directory Structure

---
+++ src/test/CardTest.java
@@ -0,0 +1,59 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CardTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNumber() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSuit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetColor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFullNumber() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSource() {
+		fail("Not yet implemented");
+	}
+
+}
